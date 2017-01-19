0e6bdc683cc5e78195d1801783efc78528500cb1
Serguei Cambour <external.z28scamb@decathlon.com>
2017-01-19 17:50:29 +0100

    Add DB constraint or user email
      * modify devise.rb default settings for email and password
      * remove schema.rb
      * declare sql schema format in application.rb
      * create a migration to apply the constraint

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,8 @@ module Shine
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # telling Rails to use SQL, rather than Ruby, for storing the schema (schema.rb was deleted)
+    config.active_record.schema_format = :sql
   end
 end
