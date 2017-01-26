56445e3fa7b60b4f48bd6579f56dcaf718651623
Serguei Cambour <external.z28scamb@decathlon.com>
2017-01-26 17:23:40 +0100

    Add Angular dependency and set it up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'angular_test/index'
+
+  get 'angular_test_controller/index'
+
   devise_for :users
   root to: "dashboard#index"
 
   resources :customers, only: [:index]
+  get "angular_test" => "angular_test#index"
 end
--- webpack/application.js
+++ webpack/application.js
@@ -1,3 +1,59 @@
-require ("application.css");
+/***
+ * Excerpted from "Rails, Angular, Postgres, and Bootstrap, Second Edition",
+ * published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material,
+ * courses, books, articles, and the like. Contact us if you are in doubt.
+ * We make no guarantees that this code is fit for any purpose.
+ * Visit http://www.pragmaticprogrammer.com/titles/dcbang2 for more book information.
+***/
+require("application.css");
 require("bootstrap/dist/css/bootstrap.css");
-console.log("Hello world!");
+var coreJS          = require('core-js');
+var zoneJS          = require('zone.js');
+var reflectMetadata = require('reflect-metadata');
+var ng              = {
+  core:                   require("@angular/core"),
+  common:                 require("@angular/common"),
+  compiler:               require("@angular/compiler"),
+  forms:                  require("@angular/forms"),
+  platformBrowser:        require("@angular/platform-browser"),
+  platformBrowserDynamic: require("@angular/platform-browser-dynamic"),
+  router:                 require("@angular/router")
+};
+//
+var AngularTestComponent = ng.core.Component({
+  selector: "shine-angular-test",
+  template: '\
+  <h2 *ngIf="salutation">Hello {{salutation}}!</h2> \
+  <form> \
+    <div class="form-group"> \
+      <label for="name">Name</label> \
+      <input type="text" id="name" class="form-control" \
+             name="name" bindon-ngModel="salutation"> \
+    </div> \
+  </form> \
+  '
+}).Class({
+  constructor: function() {
+    this.salutation = null;
+  }
+});
+
+
+var AngularTestAppModule = ng.core.NgModule({
+  imports: [ ng.platformBrowser.BrowserModule, ng.forms.FormsModule ],
+  declarations: [ AngularTestComponent ],
+  bootstrap: [ AngularTestComponent ]
+})
+.Class({
+  constructor: function() {}
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var shouldBootstrap = document.getElementById("angular-test");
+  if (shouldBootstrap) {
+    ng.platformBrowserDynamic.
+      platformBrowserDynamic().
+      bootstrapModule(AngularTestAppModule);
+  }
+});
