11ef2cf7fc1218f095469d68a36ebfb3f5b63da1
Serguei Cambour <external.z28scamb@decathlon.com>
2017-02-06 17:55:14 +0100

    Implement call to viewDetails function

--- webpack/CustomerSearchComponent.js
+++ webpack/CustomerSearchComponent.js
@@ -1,7 +1,8 @@
 var reflectMetadata = require("reflect-metadata");
 var ng = {
   core: require("@angular/core"),
-  http: require("@angular/http")
+  http: require("@angular/http"),
+  router: require("@angular/router")
 };
 
 var CustomerSearchComponent = ng.core.Component({
@@ -10,12 +11,19 @@ var CustomerSearchComponent = ng.core.Component({
 }).Class({
   constructor: [
     ng.http.Http,
-    function(http) {
+    ng.router.Router,
+    function(http, router) {
       this.customers = null;
       this.http      = http;
+      this.router    = router;
       this.keywords  = "";
     }
   ],
+
+  viewDetails: function(customer) {
+    this.router.navigate(["/", customer.id]);
+  },
+
   search: function($event) {
     var self = this;
     self.keywords = $event;
