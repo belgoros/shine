3818da745986fbb8a67b66ee1876bb15368ace29
Serguei Cambour <external.z28scamb@decathlon.com>
2017-05-07 11:59:00 +0200

    Extract credit card info into separate component
    - add a fake controller to load credit card data
    - add a fake controller route
    - modify CreditCardComponent.js (use fetch, ngOnChange)
    - use credit_card_info properties in the views
    - add #customer_id, #serializable_hash to CusomerDetail model

--- webpack/CreditCardComponent.js
+++ webpack/CreditCardComponent.js
@@ -6,7 +6,6 @@
  * We make no guarantees that this code is fit for any purpose.
  * Visit http://www.pragmaticprogrammer.com/titles/dcbang2 for more book information.
 ***/
-var reflectMetadata = require("reflect-metadata");
 var ng = {
   core:   require("@angular/core"),
   http:   require("@angular/http"),
@@ -14,12 +13,43 @@ var ng = {
 
 var CreditCardComponent = ng.core.Component({
   selector: "shine-credit-card",
-  template: require("./CreditCardComponent.html")
+  template: require("./CreditCardComponent.html"),
+  inputs: [
+    "cardholder_id"
+  ]
 }).Class({
   constructor: [
-    function() {
+    ng.http.Http,
+    function(http) {
+      this.cardholder_id = null;
+      this.credit_card_info = null;
+      this.http = http;
     }
-  ]
+  ],
+  ngOnChanges: function(changes) {
+    if (changes.cardholder_id) {
+      if (changes.cardholder_id.currentValue) {
+        this.cardholder_id = changes.cardholder_id.currentValue;
+        this.fetchCreditCardInfo();
+      }
+      else {
+        this.cardholder_id = null;
+        this.credit_card_info = null;
+      }
+    }
+  },
+  fetchCreditCardInfo: function() {
+    var self = this;
+    self.http.get("/credit_card_info/" + self.cardholder_id).
+      subscribe(
+        function(response) {
+          self.credit_card_info = response.json().credit_card_info;
+        },
+        function(response) {
+          window.alert(response);
+        }
+    );
+  }
 });
 
 module.exports = CreditCardComponent;
